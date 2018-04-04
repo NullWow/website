031522a92ec13cb1b567ebc8babaaac0ba0976ca
boveloco <boveloco@gmail.com>
2018-04-03 22:16:10 -0300

    parser class, race and gender

--- js/onlinePlayers.js
+++ js/onlinePlayers.js
@@ -1,14 +1,45 @@
+var Races = {
+    1: 'Human',
+    2: 'Orc',
+    3: 'Dwarf',
+    4: 'Night Elf',
+    5: 'Undead',
+    6: 'Tauren',
+    7: 'Gnome',
+    8: 'Troll',
+    10: 'Blood Elf',
+    11: 'Drainei'
+};
+
+var Class = {
+    1: 'Warrior',
+    2: 'Paladin',
+    3: 'Hunter',
+    4: 'Rogue',
+    5: 'Priest',
+    6: 'Deathknight',
+    7: 'Shaman',
+    8: 'Mage',
+    9: 'Warlock',
+    11: 'Druid'
+};
+
+var Gender = {
+    0: 'img/si-glyph-male.svg',
+    1: 'img/si-glyph-female.svg'
+};
+
 var populateTable = function(elements){
     for(var i = 0; i < elements.length; i++){
         $('#table-content')
         .append($('<tr>')
             .append($('<th>').attr('scope', "row").append($('<p>').text(i)))
-            .append($('<td>').append($('<p>').text(elements[i][0])))
-            .append($('<td>').append($('<p>').text(elements[i][1])))
-            .append($('<td>').append($('<p>').text(elements[i][2])))
-            .append($('<td>').append($('<p>').text(elements[i][3])))
-            .append($('<td>').append($('<img>').attr('src', elements[i][4] == 0 ? 'img/si-glyph-male.svg' : 'img/si-glyph-female.svg')))
-            .append($('<td>').append($('<p>').text(elements[i][5])))
+            .append($('<td>').text(elements[i][0]))
+            .append($('<td>').text(Races[elements[i][1]]))
+            .append($('<td>').text(elements[i][2]))
+            .append($('<td>').text(Class[elements[i][3]]))
+            .append($('<td>').append($('<img>').attr('src', Gender[elements[i][4]])))
+            .append($('<td>').text(elements[i][5]))
         )
     }
 }
