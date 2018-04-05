f5effc7824a75d63936eb9e38440f1c130b7ebda
André Rennó Bovendorp <andre.bovendorp@inatel.br>
2018-04-05 16:51:48 -0300

    Componentização do site

---
+++ js/load.js
@@ -0,0 +1,12 @@
+var loadIndex = function(){
+    $('#content').load('components/content/index.html');
+    $('#onlineListLink').removeClass('active');
+    $('#homeLink').addClass('active');
+};
+
+var loadList = function(){
+    $('#homeLink').removeClass('active');
+    $('#onlineListLink').addClass('active');
+    $('#content').load('components/content/onlineList.html');
+    // getListPlayerOnline();
+};
