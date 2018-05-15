9718482d43ee674876736410ad1ae3bda984a377
Andre Bovendorp <boveloco@gmail.com>
2018-05-15 09:24:46 -0300

    Merge all requests to one function doSendRequest()

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
+/**
+ * Show a toastr!
+ * @param {String} type Type: success|error|warning|info
+ * @param {String} head Header text!
+ * @param {String} text Text that will appea
+ * @param {Number} hide Milliseconds toastr will be visible!
+ */
 var doToastr = function(type, head, text, hide = 2000){
     $.toast({
         heading: head,
@@ -5,7 +12,30 @@ var doToastr = function(type, head, text, hide = 2000){
         showHideTransition: 'slide',
         icon: type.toLowerCase(),
         position: 'bottom-right',
-        stack: false,
+        stack: true,
         hideAfter: hide
     });
+}
+
+var doSendRequest = function(obj) {
+    var header          = Cookies.get('NULLWOW-SESSION');
+    var options         = {};
+    options.type        = obj.type;
+    options.url         = obj.url;
+    options.error       = obj.error;
+    options.success     = obj.success;
+
+    if(obj.forcedOnline){
+        if(!header){
+            return doToastr('warning', 'Logged-out', 'Você precisa estar logado para fazer essa requisição!');
+        }
+    }
+    if(header){
+        options.beforeSend = function(request){ request.setRequestHeader("NULLWOW-SESSION", Cookies.get('NULLWOW-SESSION')); }
+    }
+    if(obj.type != 'get' && obj.data != null){
+        options.data    = obj.data;
+    }
+
+    $.ajax(options);
 }
--- js/user.js
+++ js/user.js
@@ -1,88 +1,92 @@
 var login = function() {
-    $.ajax({
+    var error = function(ret) {
+        return doToastr('warning', 'Login Failed' , 'Usuário ou senha inválidos');
+    };
+
+    var success = function(resp){
+        if(resp.login == false || resp.error == true){
+            return doToastr('warning', 'Login Failed' , 'Usuário ou senha inválidos');
+
+            // return $('#loginMessage').text('Usuario ou senha inválidos!');
+        }
+        Cookies.set('NULLWOW-SESSION', resp.session, { expires: 1/24 });
+        USER_LOGGED = resp.user;
+        $('#loginButton').load('components/login/loggedIn.html');
+        return doToastr('success', 'Login Success' , 'Seja bem vindo: ' + USER_LOGGED.username + '!');
+    };
+    var options = {
         type: 'POST',
         data: $('#loginForm').serialize(),
         url: API_URI + 'session/',
-        error: function(ret){
-                return doToastr('warning', 'Login Failed' , 'Usuário ou senha inválidos');
-
-        },
-        success: function(resp){
-            if(resp.login == false || resp.error == true){
-                return doToastr('warning', 'Login Failed' , 'Usuário ou senha inválidos');
-
-                // return $('#loginMessage').text('Usuario ou senha inválidos!');
-            }
-            Cookies.set('NULLWOW-SESSION', resp.session, { expires: 1/24 });
-            USER_LOGGED = resp.user;
-            $('#loginButton').load('components/login/loggedIn.html');
-            return doToastr('success', 'Login Success' , 'Seja bem vindo: ' + USER_LOGGED.username + '!');
-        }
-    });
+        error: error,
+        success: success
+    };
+    doSendRequest(options);
 };
 
 var doGetSession = function(){
-    $.ajax({
-        type: 'GET',
-        url: API_URI + 'session/',
-        beforeSend: function(request) {
-            request.setRequestHeader("NULLWOW-SESSION", Cookies.get('NULLWOW-SESSION'));
-        },
-        error: function(ret){
-    //        $('#loginButton').load('components/login/loggedOut.html');
-            USER_LOGGED = resp.user;                    
-            return doToastr('warning', 'Login!' , 'Sessão expirou, faça login novamente!');
-        },
-        success: function(resp){
-            if(resp.error == true){
-                return;
-            }
-            if(resp.session != null) {
-                USER_LOGGED = resp.user;
-                $('#loginButton').load('components/login/loggedIn.html');
-                return doToastr('success', 'Login!' , 'Seja bem vindo: ' + USER_LOGGED.username + '!');
-            }
+    var success = function(resp){
+        if(resp.error == true){
+            console.error(resp);
+            return;
         }
-    });
+        if(resp.session != null) {
+            USER_LOGGED = resp.user;
+            $('#loginButton').load('components/login/loggedIn.html');
+            return doToastr('success', 'Login!' , 'Seja bem vindo: ' + USER_LOGGED.username + '!');
+        }
+    };
+
+    var options = {
+        type: 'GET',
+        url: API_URI + 'session',
+        success: success
+    };
+
+    doSendRequest(options);
 }
 
 var doLogout = function(){
-    $.ajax({
-        type: 'GET',
-        url: API_URI + 'session/logout',
-        beforeSend: function(request) {
-            request.setRequestHeader("NULLWOW-SESSION", Cookies.get('NULLWOW-SESSION'));
-        },
-        error: function(ret){
+    var error =  function(ret){
+        return doToastr('warning', 'Logout Failed' , 'Erro no logout, fale com um administrador!');
+    };
+
+    var success = function(resp){
+        if(resp.error == true){
             return doToastr('warning', 'Logout Failed' , 'Erro no logout, fale com um administrador!');
-        },
-        success: function(resp){
-            if(resp.error == true){
-                return doToastr('warning', 'Logout Failed' , 'Erro no logout, fale com um administrador!');
-            }
-            Cookies.remove('NULLWOW-SESSION');
-            USER_LOGGED = null;
-            $('#loginButton').load('components/login/loggedOut.html');
-            $('#content').load('components/content/index.html')
-            return doToastr('success', 'Logout' , 'Você saiu com Sucesso!');
         }
-    });
+        Cookies.remove('NULLWOW-SESSION');
+        USER_LOGGED = null;
+        $('#loginButton').load('components/login/loggedOut.html');
+        $('#content').load('components/content/index.html')
+        return doToastr('success', 'Logout' , 'Você saiu com Sucesso!');
+    }
+
+    var options = {
+        type : 'GET',
+        url: API_URI + 'session/logout',
+        error: error,
+        success: success
+    };
+   
+    doSendRequest(options);
 }
 
 var doGetChar = function(){
-    if(!USER_LOGGED){
-        return doToastr('warning', 'Error', 'Você precisa estar logado para fazer qualquer compra na store!');
-    }
-    $.ajax({
+    var success = function(resp){
+        populateCharactersStore(resp.characters);
+    };
+    var error = function(ret){
+        doToastr('warning', 'Error', 'Você não está logado!', 5000);
+    };
+    var obj = {
         type: 'GET',
         url: API_URI + 'user/char/' + USER_LOGGED.id,
-        error: function(ret){
-            doToastr('warning', 'Error', 'Você não está logado!', 5000);
-        },
-        success: function(resp){
-            populateCharactersStore(resp.characters);
-        }
-    });
+        error: error,
+        success: success,
+        forcedOnline: true
+    };
+    doSendRequest(obj);
 }
 
 var doRecoverPassword = function() {
@@ -94,8 +98,6 @@ var doRecoverPassword = function() {
         error: function(){
             $('#recoverPasswordForm').trigger('reset');
             $('#recoverPasswordButton').prop('disabled', false);
-    
-            
             return doToastr('warning', 'Erro Recuperar Senha' , 'Email não existe!');            
         },
         success: function(){
